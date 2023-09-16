cd5cce8e08aa7ed7a45fc69847c547f61c0df7ac
mecencio <nicomxd22@gmail.com>
2023-09-16 13:18:24 -0300

    add categories

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,17 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import CartWidget from "./CartWidget.jsx"
-
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import CartWidget from "./CartWidget.jsx";
 
 function Navbar() {
+  const [anchorElCat, setAnchorElCat] = React.useState(null);
+
+  const handleOpenCatMenu = (event) => {
+    setAnchorElCat(event.currentTarget);
+  };
+  const handleCloseCatMenu = () => {
+    setAnchorElCat(null);
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -34,32 +45,61 @@ function Navbar() {
             ECOM
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              key={"Home"}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Home
+            </Button>
+            <Box sx={{ flexGrow: 0 }}>
               <Button
-                key={"Home"}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                Home
-              </Button>
-              <Button
+                onClick={handleOpenCatMenu}
                 key={"Categories"}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
                 Categories
               </Button>
-              <Button
-                key={"Products"}
-                sx={{ my: 2, color: "white", display: "block" }}
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElCat}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElCat)}
+                onClose={handleCloseCatMenu}
               >
-                Products
-              </Button>
-              <Button
-                key={"Contact us"}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                Contact us
-              </Button>
+                <MenuItem key={"footwear"} onClick={handleCloseCatMenu}>
+                  <Typography textAlign="center">Footwear</Typography>
+                </MenuItem>
+                <MenuItem key={"clothing"} onClick={handleCloseCatMenu}>
+                  <Typography textAlign="center">Clothing</Typography>
+                </MenuItem>
+                <MenuItem key={"accessories"} onClick={handleCloseCatMenu}>
+                  <Typography textAlign="center">Accessories</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+            <Button
+              key={"Products"}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Products
+            </Button>
+            <Button
+              key={"Contact us"}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Contact us
+            </Button>
           </Box>
-          <Box sx={{flexGrow: 0, mr: 4}}>
+          <Box sx={{ flexGrow: 0, mr: 4 }}>
             <CartWidget />
           </Box>
           <Box sx={{ flexGrow: 0 }}>
